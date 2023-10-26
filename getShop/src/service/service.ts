5eeb0a06fed7a875d1d90ca551d9ba009f26559b
Kira-Vishn9 <Kvishn9@gmail.com>
2023-10-26 09:13:07 -0400

    fix: try fix validation function

--- getShop/src/service/service.ts
+++ getShop/src/service/service.ts
@@ -1,19 +1,16 @@
-import Axios from 'axios';
-
 const accessKey = '1bac8e472a56c259d3a7355624a9e984';
-const country_code = 'RU';
-const format = 1;
+// const country_code = 'RU';
+// const format = 1;
 
 export const onValidNumber = (inputValue: string) => {
     const number = inputValue.replace('7', '');
-
-    return Axios
-        .get(`http://apilayer.net/api/validate?access_key=${accessKey}&number=${number}&country_code=${country_code}&format=${format}`)
-        .then((response) => {
-            console.log(response)
-            return response.data;
+    return fetch(`http://apilayer.net/api/validate?access_key=${accessKey}&number=${number}`)
+        .then(response => response.json())
+        .then(data => {
+            return data;
         })
-        .catch((error) => {
+        .catch(error => {
             console.error('Произошла ошибка при выполнении запроса:', error);
+            return { success: false, error };
         });
 }
